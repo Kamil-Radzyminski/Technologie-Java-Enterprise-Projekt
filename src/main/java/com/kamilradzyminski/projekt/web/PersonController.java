913ec091b5696bd4ef95616884650167bb6fb360
Kamil Radzymiński <radzyminski.kamil@gmail.com>
2020-12-11 16:35:32 +0100

    Add PersonController

---
+++ src/main/java/com/kamilradzyminski/projekt/web/PersonController.java
@@ -0,0 +1,83 @@
+package com.kamilradzyminski.projekt.web;
+
+import com.kamilradzyminski.projekt.dto.PersonRequest;
+import com.kamilradzyminski.projekt.dto.SearchRequest;
+import com.kamilradzyminski.projekt.dto.StatisticsResponse;
+import com.kamilradzyminski.projekt.service.PersonService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+
+@Controller
+public class PersonController {
+
+    final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    //TODO Strona główna
+    @GetMapping
+    public String homepage() {
+        return "index";
+    }
+
+    //TODO Wyświetlenie wszystkich osób
+    @GetMapping("/persons")
+    public String persons(Model model) {
+        model.addAttribute("personsList", Collections.emptyList());
+        return "persons";
+    }
+
+    //TODO Formularz dodawania osób
+    @GetMapping("/persons/new")
+    public String personForm(Model model) {
+        model.addAttribute("person", new PersonRequest());
+        return "personForm";
+    }
+
+    //TODO Formularz edycji osób
+    @GetMapping("/persons/edit")
+    public String personFormEdit(Model model) {
+        model.addAttribute("person", new PersonRequest());
+        return "personForm";
+    }
+
+    //TODO Obsługa dodania osoby z formularza
+    @PostMapping("/persons/new")
+    public String personFormSubmit(@ModelAttribute PersonRequest person) {
+        return "redirect:/persons";
+    }
+
+    //TODO Obsługa edycji osoby z formularza
+    @PostMapping("/persons/edit")
+    public String personFormEditSubmit(@ModelAttribute PersonRequest person) {
+        return "redirect:/persons";
+    }
+
+    //TODO Usuwanie osoby
+    @GetMapping("/persons/delete/{id}")
+    public String personDeleteUser(@PathVariable("id") int id) {
+        return "redirect:/persons";
+    }
+
+    // TODO Wyszukiwarka po danym polu
+    @GetMapping("/persons/search")
+    public String searchPerson(Model model) {
+        model.addAttribute("search", new SearchRequest());
+        return "personSearch";
+    }
+
+    // TODO Statystyki osób
+    @GetMapping("/persons/statistics")
+    public String personStatistics(Model model) {
+        model.addAttribute("statistics", new StatisticsResponse());
+        return "personStatistics";
+    }
+}
---
+++ src/main/java/com/kamilradzyminski/projekt/web/rest/PersonRestController.java
@@ -0,0 +1,28 @@
+package com.kamilradzyminski.projekt.web.rest;
+
+import com.kamilradzyminski.projekt.dto.PersonResponse;
+import com.kamilradzyminski.projekt.service.PersonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonRestController {
+
+    final PersonService personService;
+
+    public PersonRestController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    // TODO Wyświetlanie danej osoby w postaci JSON'a
+    @CrossOrigin
+    @GetMapping(value = "/api/persons/{id}")
+    public ResponseEntity<PersonResponse> getNoteById(@PathVariable String id){
+        return new ResponseEntity<>(new PersonResponse(), HttpStatus.OK);
+    }
+
+}
