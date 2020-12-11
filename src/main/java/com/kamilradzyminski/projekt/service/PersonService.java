3ae593d0c4b1c47b879a209ef7450bba7da24b77
Kamil Radzymiński <radzyminski.kamil@gmail.com>
2020-12-11 15:14:35 +0100

    Add PersonService

---
+++ src/main/java/com/kamilradzyminski/projekt/service/PersonService.java
@@ -0,0 +1,18 @@
+package com.kamilradzyminski.projekt.service;
+
+import com.kamilradzyminski.projekt.domain.Person;
+import com.kamilradzyminski.projekt.dto.PersonRequest;
+import com.kamilradzyminski.projekt.dto.StatisticsResponse;
+import com.kamilradzyminski.projekt.domain.enums.PropertyType;
+
+import java.util.List;
+
+public interface PersonService {
+    List<Person> getAll();
+    List<Person> getByProperty(PropertyType propertyType, String query);
+    Person create(PersonRequest personRequest);
+    Person update(int id, PersonRequest personRequest);
+    void delete(int id);
+    StatisticsResponse getStatistics();
+}
+
