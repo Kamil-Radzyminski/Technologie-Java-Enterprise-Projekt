6a56140cb8fffec7da680c1ce3ad124becb912ae
Kamil Radzymiński <radzyminski.kamil@gmail.com>
2020-12-13 04:02:18 +0100

    PersonController: personForm created

--- src/main/java/com/kamilradzyminski/projekt/web/PersonController.java
+++ src/main/java/com/kamilradzyminski/projekt/web/PersonController.java
@@ -2,6 +2,7 @@ package com.kamilradzyminski.projekt.web;
 
 
 import com.kamilradzyminski.projekt.domain.Person;
+import com.kamilradzyminski.projekt.dto.PersonEditRequest;
 import com.kamilradzyminski.projekt.dto.PersonRequest;
 import com.kamilradzyminski.projekt.dto.SearchRequest;
 import com.kamilradzyminski.projekt.dto.StatisticsResponse;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PersonController {
@@ -47,11 +49,18 @@ public class PersonController {
         return "personForm";
     }
 
-    //TODO Formularz edycji osób
-    @GetMapping("/persons/edit")
-    public String personFormEdit(Model model) {
-        model.addAttribute("person", new PersonRequest());
-        return "personForm";
+    //Formularz edycji osób
+    @GetMapping("/persons/edit/{id}")
+    public String personFormEdit(@PathVariable("id") int id, Model model) {
+        Optional<Person> optionalPerson = personService.getById(id);
+        if(optionalPerson.isPresent()) {
+            Person person = optionalPerson.get();
+            PersonEditRequest oldPerson = new PersonEditRequest(person);
+            model.addAttribute("oldPerson", oldPerson);
+            return "personFormEdit";
+        } else {
+            return "404";
+        }
     }
 
     //Obsługa dodania osoby z formularza
@@ -61,9 +70,10 @@ public class PersonController {
         return "redirect:/persons";
     }
 
-    //TODO Obsługa edycji osoby z formularza
-    @PostMapping("/persons/edit")
-    public String personFormEditSubmit(@ModelAttribute PersonRequest person) {
+    //Obsługa edycji osoby z formularza
+    @PostMapping("/persons/edit/{id}")
+    public String personFormEditSubmit(@PathVariable("id") int id, @ModelAttribute PersonEditRequest person) {
+        personService.update(id, person);
         return "redirect:/persons";
     }
 
