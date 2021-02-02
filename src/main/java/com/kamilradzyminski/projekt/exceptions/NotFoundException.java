6d29b1efb6efeb7fa7b19f2767c45c78b78e9e1a
Vantor <radzyminski.kamil@gmail.com>
2021-02-02 22:47:46 +0100

    Not found exception display change

--- src/main/java/com/kamilradzyminski/projekt/exceptions/NotFoundException.java
+++ src/main/java/com/kamilradzyminski/projekt/exceptions/NotFoundException.java
@@ -2,6 +2,6 @@ package com.kamilradzyminski.projekt.exceptions;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(Long id) {
-        super("Nie znaleziono " + id);
+        super("Didn't found app with id " + id);
     }
 }
